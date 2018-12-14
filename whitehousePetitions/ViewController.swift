6db4058a2e2540d0d3a5fa54cadce147fd5cd620
MoaAlaa <moaalaa16@gmail.com>
2018-12-14 15:55:21 +0200

    Initial Commit

---
+++ whitehousePetitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whitehousePetitions
+//
+//  Created by Mohamed Alaa El-Din on 12/14/18.
+//  Copyright © 2018 Mohamed Alaa El-Din. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
